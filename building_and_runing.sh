bd59e47fb1e72de14ea87287aaefd4498a10e3b2
HeshamRagab <106036527+HeshamRagab@users.noreply.github.com>
2022-05-25 14:00:05 +0200

    add more steps required

--- building_and_runing.sh
+++ building_and_runing.sh
@@ -12,6 +12,8 @@ _cmd="`sudo docker ps -aqf \"name=^go_web_app$\"`"
 if [[ "$_cmd" != "" ]]; then
         sudo docker stop go_web_app && sudo docker rm go_web_app
 fi
+dkr_old_img_id=`sudo docker images -aq "instabug/goviolin"`
+
 
 #--------------------------#
 
@@ -20,7 +22,18 @@ git clone https://github.com/HeshamRagab/GoViolin.git
 sudo docker build -t "instabug/goviolin" .
 #build done
 
+if [[ $dkr_old_img_id  != "" ]]; then
+        sudo docker rmi -f instabug/goviolin
+fi
+
 #---------------------------#
+docker tag instabug/goviolin <aws ECR link>
+#regenratiing AWS ECR token
+sudo aws ecr get-login-password --region eu-central-1 | sudo docker login --username AWS --password-stdin <aws ECR link>
+
+#uploading image to AWS ECR
+docker push <aws ECR link>
+
 
 #starting a new one
 sudo docker run --name go_web_app -d \
